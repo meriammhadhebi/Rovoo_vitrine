70bbffa1bbc417ecc834c37249f93714013d0b11
Mhadhebi Meriam <meriam.mhadhebi@esprit.tn>
2021-10-08 00:38:54 +0200

    com

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -13,10 +13,15 @@ export const Team = (props) => {
         <div id='row'>
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 team'>
-                  <div className='thumbnail'>
+                <div key={`${d.name}-${i}`} className='col-md-4 col-sm-6 team'>
+                  <div className='thumbnail' >
                     {' '}
-                    <img src={d.img} alt='...' className='team-img' />
+                    {/* <img src={d.img} alt='...' className='team-img' /> */}
+                     <img className='team-img'
+                        src={d.img}
+                        onMouseOver={e => (e.currentTarget.src = d.hoverimg)}
+                        onMouseOut={e => (e.currentTarget.src = d.img)}
+                      />
                     <div className='caption'>
                       <h4>{d.name}</h4>
                       <p>{d.job}</p>
@@ -26,69 +31,7 @@ export const Team = (props) => {
               ))
             : 'loading'}
         </div>
-        <MDBRow className='row-cols-1 row-cols-md-3 g-4'>
-      <MDBCol>
-        <MDBCard className='h-100'>
-          <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/041.jpg'
-            alt='...'
-            position='top'
-          />
-          <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
-            <MDBCardText>
-              This is a longer card with supporting text below as a natural lead-in to additional content.
-              This content is a little bit longer.
-            </MDBCardText>
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-      <MDBCol>
-        <MDBCard className='h-100'>
-          <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/042.jpg'
-            alt='...'
-            position='top'
-          />
-          <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
-            <MDBCardText>This is a short card.</MDBCardText>
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-      <MDBCol>
-        <MDBCard className='h-100'>
-          <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/043.jpg'
-            alt='...'
-            position='top'
-          />
-          <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
-            <MDBCardText>
-              This is a longer card with supporting text below as a natural lead-in to additional content.
-            </MDBCardText>
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-      <MDBCol>
-        <MDBCard className='h-100'>
-          <MDBCardImage
-            src='https://mdbcdn.b-cdn.net/img/new/standard/city/044.jpg'
-            alt='...'
-            position='top'
-          />
-          <MDBCardBody>
-            <MDBCardTitle>Card title</MDBCardTitle>
-            <MDBCardText>
-              This is a longer card with supporting text below as a natural lead-in to additional content.
-              This content is a little bit longer.
-            </MDBCardText>
-          </MDBCardBody>
-        </MDBCard>
-      </MDBCol>
-    </MDBRow>
-    </div>
+      </div>
     </div>
   )
 }
